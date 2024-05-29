48746d92773d8ac960ea15db81c910c25e8bcf7f
KseniaZar55 <160110507+KseniaZar55@users.noreply.github.com>
2024-05-29 21:00:46 +0300

    laba9

--- 1.py
+++ 1.py
@@ -1,37 +1,30 @@
+import os
+from PIL import Image
 def h1():
-    import os
-    from PIL import Image
-    os.mkdir('mau')
-    k=0
-    for i in os.listdir('laba9'):
-        img = Image.open("C:\Users\Vladik\Documents\laba9")
+    for i in os.listdir("kartinki"):
+        img = Image.open("kartinki/" + i)
         img = img.resize((img.width // 3, img.height // 3))
-        k+=1
-        img.save("C:\Users\Vladik\Documents\laba9\mau\img" + i + ".jpg")
+        img.save("mau/img" + i)
 
 def h2():
-    k=1
-    import os
-    from PIL import Image
-    os.mkdir('pupusiki')
-    for i in os.listdir('laba9'):
+    for i in os.listdir('kartinki'):
         if i.endswith('.jpg') or i.endswith('.png'):
-            img = Image.open(i)
+            img = Image.open("kartinki/" + i)
             img = img.resize((img.width // 3, img.height // 3))
-            img.save("D:\питон\pupusiki\img" + str(k) + ".jpg")
-            k+=1
+            img.save("mau/img" + i)
 
 def h3():
     import csv
     itog=0
-    print('покупки :')
+    print('нужно купить:')
     with open('data.csv') as file:
         file = csv.reader(file)
         for i in file:
             name=i[0]
-            kol=int(i[1])
+            kolvo=int(i[1])
             cn=int(i[2])
             itog+=kolvo*cn
+            print(i[0] + "-"+ i[1] + "шт" + " " +i[2] + " руб.")
+    print("итог:" + str(itog))
 
-
-h1()
+h3()
